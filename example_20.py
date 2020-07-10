1421d8a70fa63e0829f304aaf794ad77bec45e71
Heather Nuffer <zzzuzz@gmail.com>
2020-07-09 20:02:40 -0600

    Adds another recursive example

---
+++ example_20.py
@@ -0,0 +1,35 @@
+def triple_print_num(n):
+    print(n)
+    if n == 0:
+        return
+    double_print_num(n-1)
+    double_print_num(n-1)
+    double_print_num(n-1)
+
+
+triple_print_num(3)
+
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# Time O(3^n)
